b5c60dd3e39c00a75561e64e7ae6ec46d37d52c1
dsamaddar <debumist@gmail.com>
2026-08-19 17:19:49 +0600

    upto lesson 48

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+namespace BuildingBlocks.CQRS;
+
+public interface ICommand : ICommand<Unit>
+{
+
+}
+
+public interface ICommand<out TResponse>: IRequest<TResponse>
+{
+}
+
